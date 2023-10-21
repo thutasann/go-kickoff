e024d3a95127249b96b7304df60adb1372ec8e26
thutasann <66902308+thutasann@users.noreply.github.com>
2023-10-21 16:30:11 +0630

    chore: update readme

--- src/main.go
+++ src/main.go
@@ -8,6 +8,7 @@ import (
 func main(){
 	var m1 string
 	m1 = "thuta sann"
-	fmt.Println(strings.ReplaceAll(m1, "t", "NO"))
+	fmt.Println(strings.ReplaceAll(m1, "n", "🚀"))
 	fmt.Println(strings.Split(m1, " "))
+	m1 = "Thuta Sann"
 }
