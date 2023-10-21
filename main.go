35e3e9b1e1a639f5d6e74b6e49b5d91e410f5c24
thutasann <66902308+thutasann@users.noreply.github.com>
2023-10-21 10:28:25 +0630

    chore: arrays

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt";
-
-func main()  {
-	fmt.Println("Hello, world")
+func swap(m1, m2 *int){
+	var temp int
+	temp = *m2;
+	*m2 = *m1;
+	*m1 = temp
 }
+
+// func main(){
+// 	var m1 string
+// 	m1 = "thuta sann"
+// 	fmt.Println(strings.ReplaceAll(m1, "t", "NO"))
+// 	fmt.Println(strings.Split(m1, " "))
+// }
+
+// func main()  {
+// 	m1 := 1;
+// 	m2 := 2;
+// 	fmt.Println(int(m1) + m2);
+// }
---
+++ main2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func todo()  {
+	arr := []int{1,2,3}
+	arr2 := []string{"thuta", "hsu", "pyae"}
+	arr2 = append(arr2, "kyaw")
+	fmt.Println(arr)
+	fmt.Println(arr2)
+}
+
+func main(){
+	todo();
+}
